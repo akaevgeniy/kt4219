4fbff1617160ba728dfcb011236723580f2adb37
Chgu I207 <akaevgeniy@yandex.ru>
2022-09-17 12:44:05 +0300

    Init app

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E76025',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
   },
 });
